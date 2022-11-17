4fe7ba73475eaec7baf092f53ad4268efc3f7804
grenademik <grenademik@gmail.com>
2022-11-18 00:25:43 +0545

    Login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const username="admin";
+  const password="admin";
+  const [state,setState] = useState([]);
+  
+  const authentication = (user, pass) => {
+    if(user===username && pass===password){
+    return setState("Login Sucess");
+    } else {
+      return setState("Failed");
+    }
+  } 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="login-card">
+    <Apptitle/>
+      <form>
+       <Form checkAuth={authentication}/>
+       
+      </form>
+      <p>STATUS:{state}</p>
+  </div>
   );
 }
+
+function Apptitle(){
+  return (<div>
+  <h1>Login Form</h1>
+  </div>
+  );
+}
+
+function Form({checkAuth}){
+  const [user,setUser] = useState("");
+  const [pass,setPass] = useState("");
+  const handleUser = (event) =>{
+    const userValue= event.target.value;
+    setUser((_) =>userValue)
+  }
+  const handlePass = (event) =>{
+    const passValue= event.target.value;
+    setPass((_) =>passValue)
+  }
+
+  const auth = () => {
+    checkAuth(user,pass);
+  }
+  return(<>
+    <label>
+    Username:
+    <input onChange={handleUser} value={user}/>
+  </label><br/>
+  <label>
+          Password:
+          <input type="password" onChange={handlePass}/>
+        </label><br/>
+  <button onClick={auth}>Submit</button>
+  </>
+  );
+}
+
 
 export default App;
